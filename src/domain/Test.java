f416574a7f778463c004cd8ac4153214d5e2c2c9
LylYorick <1352095235@qq.com>
2017-09-14 17:35:37 +0800

    C13:

---
+++ src/domain/Test.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("C4");
+		System.out.println("C1");
+		System.out.println("C11");
+		System.out.println("C2");
+		System.out.println("C5");
+		System.out.println("C7");
+		System.out.println("C8");
+
+		System.out.println("C10");
+		
+		System.out.println("C13");
+	}
+}
